37c4253269a8e5c2dbd0aaff3a1886b393eb4475
Richard Li <richardjiali674@gmail.com>
2025-07-01 16:37:03 -0700

    fix 1:8000 error

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,17 +5,18 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    host: true,
     port: 3000,
     proxy: {
       // Proxy websocket and API calls to backend (for dev convenience)
       '/socket.io': {
-        target: 'http://localhost:8000',
+        target: 'http://backend:8000',
         ws: true
       },
       '/auth': {
-        target: 'http://localhost:8000',
-        changeOrigin: true
+        target: 'http://backend:8000',
+        changeOrigin: true,
+        secure: false,
       }
     }
   }
